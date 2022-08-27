1509599611bfbdbafb4f55cc3a24183ad9383888
iqvw64e.sys <112026923+pwned-sys@users.noreply.github.com>
2022-08-27 17:25:22 +0200

    Update variables.cpp

--- Lesson 1: Variables/variables.cpp
+++ Lesson 1: Variables/variables.cpp
@@ -7,7 +7,11 @@ int main()
     string personsname = "John"; // we first define a string. in this case "personname". if we change the name "John" to something else the printed text from below will change too
     int age; // here we create an integer
     age = 5; // we now set the integer value in this case "age" to 5. the same happens as for changing the name. the printed text from below changes.
-    std::cout << "Hello My Name is " << personsname << " I am " << age << " Years Old" << endl;
+    std::cout << "Hello My Name is " << personsname << endl;
+    std:cout << " I am " << age << " Years Old" << endl;
+    
+    personsname = "Brayden" 
+    std::cout << "He Liked A Paster Named " << personsname << endl; // see how i used "personsname" again, its cool isnt it? 
 
     return 0;
 }

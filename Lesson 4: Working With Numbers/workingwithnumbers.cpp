469df4337bfc153dcf243f4daae54f09e17d026d
iqvw64e.sys <112026923+pwned-sys@users.noreply.github.com>
2022-08-28 01:16:52 +0200

    Update workingwithnumbers.cpp

--- Lesson 4: Working With Numbers/workingwithnumbers.cpp
+++ Lesson 4: Working With Numbers/workingwithnumbers.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <cmath> // for math functions
 
 int main()
 {
+  std::cout << 5 + 7; // this wont print "5 + 7" but it will actually calculate and give you the results
+  
+  std::cout << 10 % 3; // gives us the remainder of the division
+  
+  std::cout << 10 + 7 * 10; // the result is "80" if you got it wrong start to learn in school not github
+  
+  std::cout << (10 + 7) * 10; // the result is "170" if you got it wrong start to learn in school not github
+  
+  std::cout << 5 + 9.9; // yes the result is "14.9" ur getting better at maths
+  
+  std::cout << 10 / 3; // haha got you! it will print "3" not "3.3333"
+
+  std::cout << 10.0 / 3.0; // we added decimals so now it will print "3.3333" instead of "3"
+  
+  std::cout pow(2, 5); // it will raise 2 to power 5
+  
+  std::cout round(4.6); // the result is "5"
+  
   return 0;
 }

c1ee6b294f3120af0f76eb5c0aa3860b15e0347c
iqvw64e.sys <112026923+pwned-sys@users.noreply.github.com>
2022-08-28 01:30:51 +0200

    Update calculator.cpp

--- Lesson 6: Your First Program - A Calculator/calculator.cpp
+++ Lesson 6: Your First Program - A Calculator/calculator.cpp
@@ -3,14 +3,14 @@
 
 int main()
 {
-  double num1, num2
-  std::cout << "Enter First Number: "
-  std::cin >> num1
+  double num1, num2;
+  std::cout << "Enter First Number: " 
+  std::cin >> num1; // getting users input for the first number
   
   std::cout << "Enter The Second Number: "
-  std::cin >> num2
+  std::cin >> num2;  // getting users input for the second number
   
-  std::cout << "Your Numbers Added Are Equal To: " << num1 + num2 ;
+  std::cout << "Your Numbers Added Are Equal To: " << num1 + num2;  // adding both of numbers and displaying them
 
   return 0;
 }

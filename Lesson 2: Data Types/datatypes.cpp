079107f390412ae0c10d5d3d42cdb5dae424d077
iqvw64e.sys <112026923+pwned-sys@users.noreply.github.com>
2022-08-28 00:45:42 +0200

    Update datatypes.cpp

--- Lesson 2: Data Types/datatypes.cpp
+++ Lesson 2: Data Types/datatypes.cpp
@@ -6,6 +6,7 @@ int main()
     string academy = "Brayden's Academy";
     int age = "25"; // used to display full numbers without decimals
     double gpa = "3.3"; // used to display numbers with decimals, you could use "float gpa = "3.3";" here too
+    bool isGay = false; // bool can only take two values: 0 = false and 1 = true
 
     std::cout << grade << endl; // you could just do "std::cout << "A" << endl;" but its easier to store your datas in variables
     return 0;

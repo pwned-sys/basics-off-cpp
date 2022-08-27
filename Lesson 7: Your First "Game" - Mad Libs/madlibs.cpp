3db2e30828b2b9acdd9f2843da5bcf85ea664350
iqvw64e.sys <112026923+pwned-sys@users.noreply.github.com>
2022-08-28 01:45:22 +0200

    Create madlibs.cpp

---
+++ Lesson 7: Your First "Game" - Mad Libs/madlibs.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+  string color, plurar, person
+  std::cout << "Enter A Color: " 
+  getline(cin, color);
+  
+  std::cout << "Enter A Plural Noun: " 
+  getline(cin, plural);
+  
+  std::cout << "Enter The Name Of The Person You Love: " 
+  getline(cin, person);
+
+  std:cout << "Roses are " << color << endl;
+  std:cout << plurar <<< "are blue" << endl;
+  std:cout << "I Love" << person << endl;
+  return 0;
+}

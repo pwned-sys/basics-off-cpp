3ba0c3376fdd90383cc796f1c36b88bfc1828eeb
iqvw64e.sys <112026923+pwned-sys@users.noreply.github.com>
2022-08-28 00:39:40 +0200

    Create datatypes.cpp

---
+++ Lesson 2: Data Types/datatypes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    char grade = 'A'; // used to declare character type variables
+    string academy = "Brayden's Academy";
+    int age = "25"; // used to display full numbers without decimals
+    double gpa = "3.3"; // used to display numbers with decimals, you could use "float gpa = "3.3";" here too
+
+    std::cout << grade << endl; // you could just do "std::cout << "A" << endl;" but its easier to store your datas in variables
+    return 0;
+}
